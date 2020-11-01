b7b1edc9809502f93c6fdd914b06e56b7ee41f7b
DSRamirez <d.s.ramirez@outlook.com>
2020-11-01 17:08:42 -0300

    Front
    Front

--- Pelu-Shift/Pelu-Shifts/Pelu-Shifts/Turnero.cs
+++ Pelu-Shift/Pelu-Shifts/Pelu-Shifts/Turnero.cs
@@ -12,25 +12,39 @@ namespace Pelu_Shifts
 {
     public partial class Turnero : Form
     {
+        #region propiedades
+
+        DataTable lista = new DataTable();
+        int turno = 0;
+
+
+        #endregion
+
+
+
+        #region Constructor
+
         public Turnero()
         {
             InitializeComponent();
             lista.Columns.Add("Turno", typeof(int));
-            lista.Columns.Add("Servicio",typeof(string));
-            lista.Columns.Add("Precio",typeof(string));
+            lista.Columns.Add("Servicio", typeof(string));
+            lista.Columns.Add("Precio", typeof(string));
             lista.Columns.Add("Horario", typeof(string));
-            lista.Columns.Add("Peluquero",typeof(string));
+            lista.Columns.Add("Peluquero", typeof(string));
 
             dg.DataSource = lista;
         }
 
-        DataTable lista = new DataTable();
-        int turno = 0;
-        
 
-        private void button1_Click(object sender, EventArgs e)
+        #endregion
+
+
+        #region eventos
+
+        private void button1_Click(object sender, EventArgs e) // boton de reservar turno
         {
-            
+
             turno = turno + 1;
 
             lista.Rows.Add();
@@ -47,13 +61,24 @@ namespace Pelu_Shifts
 
         }
 
+        private void button2_Click(object sender, EventArgs e) // boton de salir 
+        {
+            Application.Exit();
+        }
+
+        #endregion
+
+
+
+        #region metodos
+
         private void Peluquero()
         {
             if (txthorarios.Text.Trim() == "Martes: 9:00hs" || txthorarios.Text.Trim() == "Martes: 12:00hs" || txthorarios.Text.Trim() == "Martes: 17:00hs" || txthorarios.Text.Trim() == "Martes: 20:00hs" || txthorarios.Text.Trim() == "Viernes: 9:00hs" || txthorarios.Text.Trim() == "Viernes: 12:00hs" || txthorarios.Text.Trim() == "Viernes: 17:00hs" || txthorarios.Text.Trim() == "Viernes: 20:00hs")
             {
                 lista.Rows[lista.Rows.Count - 1]["Peluquero"] = "Peluquero 2";
             }
-            else if (txthorarios.Text.Trim() == "Miercoles: 9:00hs" || txthorarios.Text.Trim() == "Miercoles: 12:00hs"|| txthorarios.Text.Trim() == "Miercoles: 17:00hs" || txthorarios.Text.Trim() == "Miercoles: 20:00hs" || txthorarios.Text.Trim() == "Jueves: 9:00hs" || txthorarios.Text.Trim() == "Jueves: 12:00hs" || txthorarios.Text.Trim() == "Jueves: 17:00hs" || txthorarios.Text.Trim() == "Jueves: 20:00hs")
+            else if (txthorarios.Text.Trim() == "Miercoles: 9:00hs" || txthorarios.Text.Trim() == "Miercoles: 12:00hs" || txthorarios.Text.Trim() == "Miercoles: 17:00hs" || txthorarios.Text.Trim() == "Miercoles: 20:00hs" || txthorarios.Text.Trim() == "Jueves: 9:00hs" || txthorarios.Text.Trim() == "Jueves: 12:00hs" || txthorarios.Text.Trim() == "Jueves: 17:00hs" || txthorarios.Text.Trim() == "Jueves: 20:00hs")
             {
                 lista.Rows[lista.Rows.Count - 1]["Peluquero"] = "Peluqueria 1";
             }
@@ -70,7 +95,7 @@ namespace Pelu_Shifts
             {
                 lista.Rows[lista.Rows.Count - 1]["Precio"] = "$600";
             }
-            else if (txtservicios.Text == "Peinado")
+            else if (txtservicios.Text == "Peinados")
             {
                 lista.Rows[lista.Rows.Count - 1]["Precio"] = "$800";
             }
@@ -82,10 +107,17 @@ namespace Pelu_Shifts
             {
                 lista.Rows[lista.Rows.Count - 1]["Precio"] = "$500";
             }
-        }
-        private void Form1_Load(object sender, EventArgs e)
-        {
+
+
+           
+
+
 
         }
+        #endregion
+
     }
+
+
+
 }
